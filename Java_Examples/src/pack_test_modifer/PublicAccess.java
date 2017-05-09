0ae029b85058bd4b11fd58979298a60e9cc7aac3
farah <farah2015eng@gmail.com>
2017-05-08 18:02:07 -0700

    Second_Commit_Eclipse_Git

---
+++ Java_Examples/src/pack_test_modifer/PublicAccess.java
@@ -0,0 +1,14 @@
+package pack_test_modifer;
+import packexample_4.*;
+class PublicAccess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 PublicClass obj = new PublicClass();
+
+		    obj.show();
+
+	}
+
+}
---
+++ Java_Examples/src/packexample_4/PublicClass.java
@@ -0,0 +1,14 @@
+package packexample_4;
+
+public class PublicClass {
+	
+	
+	 public void show()
+
+	  {
+
+	    System.out.println("Public class");
+
+	  }
+
+}
